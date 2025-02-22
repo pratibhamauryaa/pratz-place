441f92231eaad0fea88ea955cf923ee3fc6029f8
Pratibha Maurya <pratibham@xtracapindia.com>
2025-02-22 17:26:24 +0530

    initilize the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>This is going to be the start of my restaurant application </h1>
+    </>
+  )
+}
+
+export default App
